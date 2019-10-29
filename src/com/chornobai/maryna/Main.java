579e8a3d8449b04e5937f1ab6c5d7ec2a05f1802
Maryna1997 <marishachornobai1997@gmail.com>
2019-10-29 22:00:13 +0200

    Check style

--- src/com/chornobai/maryna/Main.java
+++ src/com/chornobai/maryna/Main.java
@@ -3,7 +3,7 @@ package com.chornobai.maryna;
 public class Main {
 
     public static void main(String[] args) throws HobbyException {
-        
+
         Football hobby1 = new Football("Football", 6, 25);
         Running hobby2 = new Running("Running", 8, 200);
         Football hobby3 = new Football("Football", 10, 83);
